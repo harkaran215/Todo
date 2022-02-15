58ebcf3be9927b031a31729654df36fad29dc1d2
harkaran <harkaran215@gmail.com>
2022-02-15 20:35:33 +0530

    modified config

--- Todo-project/Todo/settings.py
+++ Todo-project/Todo/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
 STATICFILES_DIRS =[BASE_DIR / 'todo_frontend/build/static']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
